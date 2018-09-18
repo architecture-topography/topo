7689a4f5ef223ab33319300794d8df2a1d1ecc83
Harsh <hpandya@thoughtworks.com>
2018-09-18 11:54:16 +1000

    HP|JO - Order capability in Domain

--- src/client/components/Domain.js
+++ src/client/components/Domain.js
@@ -9,14 +9,16 @@ export default class Domain extends Component {
 
     render() {
         const {name, description, capabilities } = this.props;
-
+        let orderedCapabilities = capabilities.sort((a, b) => {
+            return a.order > b.order
+        })
         return (
             <div>
                 <span className="domain-name"> { name } </span>
                 <span className="domain-desc"> { description } </span>
                 {
-                    capabilities.map(capability => {
-                        return <span key={ capability.order } className="domain-cap"> { capability.name } </span>
+                    orderedCapabilities.map(capability => {
+                        return <span key={ capability.order } className="domain-cap">{ capability.name }</span>
                     })
                 }
             </div>
--- tests/components/Domain.test.js
+++ tests/components/Domain.test.js
@@ -18,4 +18,20 @@ describe('Domain',()=>{
         expect(wrapper.find('.domain-desc').children().at(1).text()).toEqual(description)
         expect(wrapper.find('.domain-cap')).toHaveLength(3)
     })
+
+    it('renders capability in the correct order', () => {
+        const name = "Domain 1"
+        const description = "Description 1"
+        const capabilities = [
+            {"name": "Capability 3", "order": 3},
+            {"name": "Capability 1", "order": 1},
+            {"name": "Capability 2", "order": 2}
+        ]
+
+        const wrapper = shallow(<Domain name={name} description={description} capabilities={capabilities}/>)
+        
+        expect(wrapper.find('.domain-cap').at(0).text()).toEqual("Capability 1")
+        expect(wrapper.find('.domain-cap').at(1).text()).toEqual("Capability 2")
+        expect(wrapper.find('.domain-cap').at(2).text()).toEqual("Capability 3")
+    })
 });
