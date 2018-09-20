90a17fd581b0844dda98f49558327e42097258ab
Lex Toumbourou <lextoumbourou@gmail.com>
2018-09-20 16:17:49 +1000

    Error handling.

--- src/client/components/CapabilityView.js
+++ src/client/components/CapabilityView.js
@@ -13,21 +13,32 @@ export default class CapabilityView extends Component {
     render() {
         const { capabilities, capabilityId } = this.props
         const capability = capabilities[capabilityId];
-        
-        return (
+
+        if (!capability) {
+          return (
             <Grid columns="equal">
               <Grid.Column>
-                <Segment className="capability-name-title">
-                  <Header>{ capability.name }</Header>
+                <Segment className="capability-name-title capability-missing">
+                  <Header>Capability missing</Header>
                 </Segment>
-
-                {
-                  capability.systems.map((system, index) => {
-                    return <Segment key={ index } inverted color="blue" tertiary className="domain-cap" content={system.name} />
-                  })
-                }
               </Grid.Column>
             </Grid>
           )
+        }
+
+        return (
+          <Grid columns="equal">
+            <Grid.Column>
+              <Segment className="capability-name-title">
+                <Header>{ capability.name }</Header>
+              </Segment>
+              {
+                capability.systems.map((system, index) => {
+                  return <Segment key={ index } inverted color="blue" tertiary className="domain-cap" content={system.name} />
+                })
+              }
+            </Grid.Column>
+          </Grid>
+        )
     }
 }
