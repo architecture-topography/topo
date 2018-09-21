3f33e7be7432e0a8f31493d2e9de103ea0bb3d89
Harsh <hpandya@thoughtworks.com>
2018-09-21 15:23:52 +1000

    HP - Fix test

--- tests/client/components/PlatformView.test.js
+++ tests/client/components/PlatformView.test.js
@@ -4,34 +4,36 @@ import {shallow} from 'enzyme';
 
 describe('PlatformView', () => {
     it('renders a platform',()=>{
-        const platforms = [
-            {
-                "name": "platform 1",
-                "domains": [
-                    {
-                        "name": "Domain 1",
-                        "description": "Description 1",
-                        "capabilities": [
-                            {"name": "Capability 1", "order": 1},
-                            {"name": "Capability 2", "order": 2},
-                            {"name": "Capability 3", "order": 3}
-                        ]
-                        },
+        const treasureMapData = {
+            'platforms': [
+                {
+                    "name": "platform 1",
+                    "domains": [
                         {
-                        "name": "Domain 2",
-                        "description": "Description 2",
-                        "capabilities": [
-                            {"name": "Capability 1", "order": 1},
-                            {"name": "Capability 2", "order": 2},
-                            {"name": "Capability 3", "order": 3}
-                        ]
-                    }
-                ]
-            }
-        ]
+                            "name": "Domain 1",
+                            "description": "Description 1",
+                            "capabilities": [
+                                {"name": "Capability 1", "order": 1},
+                                {"name": "Capability 2", "order": 2},
+                                {"name": "Capability 3", "order": 3}
+                            ]
+                            },
+                            {
+                            "name": "Domain 2",
+                            "description": "Description 2",
+                            "capabilities": [
+                                {"name": "Capability 1", "order": 1},
+                                {"name": "Capability 2", "order": 2},
+                                {"name": "Capability 3", "order": 3}
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
 
-        const wrapper = shallow(<PlatformView platforms={platforms} />)
-        expect(wrapper.find('.platform-name-title').at(0).render().text()).toEqual(platforms[0].name)
+        const wrapper = shallow(<PlatformView treasureMapData={treasureMapData} />)
+        expect(wrapper.find('.platform-name-title').at(0).render().text()).toEqual(treasureMapData.platforms[0].name)
         expect(wrapper.find('.platform-domains').length).toEqual(1)
     })
 })
