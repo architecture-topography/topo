d18b844690815013d091034dc9b32d72e9ad2758
Joey Hotz <joeyhotz1@gmail.com>
2018-09-24 17:02:34 +1000

    [J[ey] add other capabilities in system hover popup

--- src/client/components/CapabilityView.js
+++ src/client/components/CapabilityView.js
@@ -1,6 +1,6 @@
 import React ,{Component} from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Header, Segment, Divider, Container, Label, Popup, List } from 'semantic-ui-react'
+import { Grid, Header, Segment, Popup, List } from 'semantic-ui-react'
 import '../../resources/css/Topo.css'
 
 export default class CapabilityView extends Component {
@@ -8,20 +8,6 @@ export default class CapabilityView extends Component {
     static propTypes = {
         treasureMapData: PropTypes.instanceOf(Object).isRequired,
         capabilityId: PropTypes.string.isRequired
-    }
-
-    getListOfSystemAttribute(system, attribute) {
-      if (system[attribute] && system[attribute].length > 0) {
-        return (
-          <List as='ul'>
-            {system[attribute].map((val, index) => {
-              return <List.Item key={ index } as='li'>{ val }</List.Item>
-            })}
-          </List>
-        )
-      } else {
-        return "None"
-      }
     }
 
     getCapabilityFromId() {
@@ -48,79 +34,82 @@ export default class CapabilityView extends Component {
     }
 
     render() {
-        const capability = this.getCapabilityFromId();
+      const capability = this.getCapabilityFromId();
 
-        if (!capability) {
-          return (
-            <Grid columns="equal">
-              <Grid.Column>
-                <Segment className="capability-name-title capability-missing">
-                  <Header>Capability missing</Header>
-                </Segment>
-              </Grid.Column>
-            </Grid>
-          )
-        }
-
-        let systems = '';
-        if (capability.systems) {
-            systems = capability.systems.map((system, index) => {
-                return (
-                  <Popup
-                    trigger={
-                      <Segment inverted color="blue" tertiary className="domain-cap">
-                        <Header as='h3'>{system.name}</Header>
-                        {this.otherCapabilities(system.capabilities)}
-                      </Segment>
-                    }
-                    key={ index }
-                    size='small'
-                    position='right center'
-                  >
-                    <Popup.Content className='system-tech-stack'>
-                      <Header as='h3'>Primary technologies</Header>
-                      {this.getListOfSystemAttribute(system, 'primary-technologies')}
-                      <Header as='h3'>Infrastructure</Header>
-                      {this.getListOfSystemAttribute(system, 'infrastructure')}
-                    </Popup.Content>
-                  </Popup>
-                  
-                )
-            })
-        }
-
+      if (!capability) {
         return (
           <Grid columns="equal">
             <Grid.Column>
-              <Segment className="capability-name-title">
-                <Header>{ capability.name }</Header>
+              <Segment className="capability-name-title capability-missing">
+                <Header>Capability missing</Header>
               </Segment>
-              { systems }
             </Grid.Column>
           </Grid>
         )
+      }
+
+      return (
+        <Grid columns="equal">
+          <Grid.Column>
+            <Segment className="capability-name-title">
+              <Header>{ capability.name }</Header>
+            </Segment>
+            { this.getSystems(capability) }
+          </Grid.Column>
+        </Grid>
+      )
     }
 
-    otherCapabilities(capabilities) {
-      if (capabilities && capabilities.length) {
-        const capabilityOnView = this.getCapabilityFromId();
-        const capabilitiesWithoutCapabilityOnView = capabilities.filter((capability) => {
-          return capability !== capabilityOnView.name;
-        });
-  
-        if (capabilitiesWithoutCapabilityOnView && capabilitiesWithoutCapabilityOnView.length) {
-          return (
-            <Container id='other-capabilities'>
-            <Divider/>
-            <span>Other capabilities: </span>
-            {capabilitiesWithoutCapabilityOnView.map((capability, index) => {
+    getSystems(capability) {
+      let systems = '';
+      if (capability.systems) {
+          systems = capability.systems.map((system, index) => {
               return (
-                <Label key={index} size='small'>{capability}</Label>
-                );
+                <Popup
+                  trigger={
+                    <Segment inverted color="blue" tertiary className="domain-cap">
+                      <Header as='h3'>{system.name}</Header>
+                    </Segment>
+                  }
+                  key={ index }
+                  size='small'
+                  position='right center'
+                >
+                  <Popup.Content className='system-tech-stack'>
+                    <Header as='h3'>Primary technologies</Header>
+                    {this.getListOfSystemAttribute(system, 'primary-technologies')}
+                    <Header as='h3'>Infrastructure</Header>
+                    {this.getListOfSystemAttribute(system, 'infrastructure')}
+                    <Header className={'other-capabilities'} as='h3'>Other Capabilities</Header>
+                    {this.getListOfOtherCapabilities(system, capability)}
+                  </Popup.Content>
+                </Popup>
+              )
+          })
+      }
+      return systems;
+    }
+
+    getListOfOtherCapabilities(system, expandedCapability) {
+      let otherCapabilities = system.capabilities.filter((capability) => capability !== expandedCapability.name)
+      if (otherCapabilities.length <= 1) return <span id="no-capabilities-text" style={{"fontStyle": "italic"}}>No other capabilities</span>;
+      return this.getListOfSystemAttribute(system, 'capabilities', otherCapabilities);
+    }
+
+    getListOfSystemAttribute(system, attribute, optionalAttributeList) {
+
+      let attributeList = (optionalAttributeList && optionalAttributeList.length ? optionalAttributeList : system[attribute]);
+
+      if (attributeList && attributeList.length) {
+        return (
+          <List as='ul'>
+            {attributeList.map((val, index) => {
+              return <List.Item key={ index } as='li'>{ val }</List.Item>
             })}
-            </Container>
-          )
-        }  
+          </List>
+        )
+      } else {
+        return <span id={attribute + "-none"}>None</span>
       }
     }
 }
