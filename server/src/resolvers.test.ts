78573e805c3681624a5a56c170c24c5ddff74ead
Jafari Sitakange <sitajaf@gmail.com>
2019-01-11 16:09:54 +1100

    Add getPlatformis  resolver.
    Co-authored-by: Joey Hotz <jhotz@thoughtworks.com>

--- server/src/resolvers.test.ts
+++ server/src/resolvers.test.ts
@@ -1,7 +1,35 @@
 import resolvers from "./resolvers";
 
-describe("hello resolver", () => {
-  it("returns hello topo", () => {
-    expect(resolvers.Query.hello()).toEqual("Hello, Topo");
+describe("resolvers", () => {
+  describe("hello", () => {
+    it("returns hello topo", () => {
+      expect(resolvers.Query.hello()).toEqual("Hello, Topo");
+    });
+  });
+
+  describe("platforms", () => {
+    describe("getPlatforms", () => {
+      it("should return all platforms", async () => {
+        const platform = {
+          id: "e71278fa-ba70-4043-9a39-9dce0497d5a3",
+          name: "TW"
+        };
+        const stubPlatforms = [platform];
+
+        const mockQueries = {
+          findPlatforms: jest.fn().mockResolvedValue(stubPlatforms)
+        };
+        const stubContext = () => ({ queries: mockQueries });
+
+        const platforms = await resolvers.Query.getPlatforms(
+          platform,
+          null,
+          stubContext
+        );
+
+        expect(mockQueries.findPlatforms).toHaveBeenCalled();
+        expect(platforms).toEqual(stubPlatforms);
+      });
+    });
   });
 });
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,5 +1,12 @@
+import { Platform } from "./domain";
+
 export default {
   Query: {
-    hello: () => "Hello, Topo"
+    hello: () => "Hello, Topo",
+    getPlatforms: async (parent: Platform, args: null, context: Function) => {
+      const platforms = await context().queries.findPlatforms();
+
+      return platforms;
+    }
   }
 };
