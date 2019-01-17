0797ff3ae0dfa357d65854fbc6ab0192cf6d4418
Katie Peterson <katie.peterson@gmail.com>
2019-01-18 10:55:43 +1100

    [Katie|Linus] First step towards legit integration tests

--- server/src/resolvers.test.ts
+++ server/src/resolvers.test.ts
@@ -1,4 +1,18 @@
 import resolvers from "./resolvers";
+import { server } from "./server";
+import { createTestClient } from "apollo-server-testing";
+import driver from "./neo";
+import { ApolloServerBase } from "apollo-server-core";
+
+// STEP ONE
+// import server - done
+// use test client to create integration tests
+//put data into db
+
+// STEP TWO
+// work out how to connect to test database
+// work out how to clean database
+// work out how to spin up test database
 
 describe("resolvers", () => {
   describe("hello", () => {
@@ -7,30 +21,69 @@ describe("resolvers", () => {
     });
   });
 
-  describe("platforms", () => {
-    describe("getPlatforms", () => {
-      it("should return all platforms", async () => {
-        const platform = {
-          id: "0",
-          name: "TW",
-          domains: [{ id: "1", name: "test domain" }]
-        };
-        const stubPlatforms = [platform];
-
-        const mockQueries = {
-          findPlatforms: jest.fn().mockResolvedValue(stubPlatforms)
-        };
-        const stubContext = { queries: mockQueries };
-
-        const platforms = await resolvers.Query.platforms(
-          platform,
-          null,
-          stubContext
+  describe("getPlatfroms", () => {
+    const name = "Test Platform";
+    const domainName = "Test Domain";
+    it("should return all platforms", async () => {
+      const session = driver.session();
+      const { query } = createTestClient(server);
+      try {
+        await session.run(
+          `CREATE (platform:Platform { name: $name })
+          CREATE (domain:Domain { name: $domainName })
+          CREATE (platform)-[:HAS]->(domain)
+        `,
+          {
+            name,
+            domainName
+          }
         );
+      } finally {
+        session.close();
+      }
 
-        expect(mockQueries.findPlatforms).toHaveBeenCalled();
-        expect(platforms).toEqual(stubPlatforms);
+      const QUERY = `
+      query {
+        platforms{
+          name,
+          domains {
+            name
+          }
+        }
+      }
+      `;
+
+      const res = await query({
+        query: QUERY
       });
+
+      expect(res.data).toBeDefined();
+      expect(res.data ? res.data.platforms : []).toContainEqual({
+        domains: [
+          {
+            name: "Test Domain"
+          }
+        ],
+        name: "Test Platform"
+      });
+    });
+
+    afterEach(async () => {
+      const session = driver.session();
+
+      try {
+        await session.run(
+          "MATCH (platform:Platform { name: $name }) DETACH DELETE platform",
+          { name }
+        );
+        await session.run(
+          "MATCH (domain:Domain { name: $domainName }) DETACH DELETE domain",
+          { domainName }
+        );
+      } finally {
+        session.close();
+      }
+      driver.close();
     });
   });
 });
