f48f580ef2330a442e79ffc508af15f30450b310
Joey Hotz <joeyhotz1@gmail.com>
2019-01-21 14:59:23 +1100

    [Joey] fixed import statement

--- server/test/queries.test.js
+++ server/test/queries.test.js
@@ -1,6 +1,6 @@
 const { driver } = require("../src/neo");
 const { findPlatforms } = require("../src/queries")(driver);
-import { clearDb } from "./helpers/testHelper";
+const { clearDb } = require("./helpers/testHelper");
 
 describe("queries", () => {
   describe("findPlatforms with Domains", () => {
