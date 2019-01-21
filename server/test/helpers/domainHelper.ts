9e4076a1518bd5029e458c17a6d7ba34ac07a4d6
Joey Hotz <joeyhotz1@gmail.com>
2019-01-21 14:27:33 +1100

    [Joey] fixed paramater for domain helper

--- server/test/helpers/domainHelper.ts
+++ server/test/helpers/domainHelper.ts
@@ -1,10 +1,13 @@
 import { driver } from "../../src/neo";
 
-export const createTestPlatformAndDomain = async (platformName, domainName) => {
+export const createTestPlatformAndDomain = async (
+  platformName: String,
+  domainName: String
+) => {
   const session = driver.session();
   try {
     await session.run(
-      `CREATE (platform:Platform { name: $name })
+      `CREATE (platform:Platform { name: $platformName })
           CREATE (domain:Domain { name: $domainName })
           CREATE (platform)-[:HAS]->(domain)
         `,
