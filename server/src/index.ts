b22c1ce74cfd71dd6906993b039b40ae3e909ef7
Joey Hotz <joeyhotz1@gmail.com>
2019-01-21 15:32:01 +1100

    [Joey] converted index to ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ const { server } = require("./server");
 server
   .listen()
   .then(() => console.log("Server running at port http://localhost:4000"))
-  .catch(err => {
+  .catch((err: any) => {
     console.error(err);
     process.exit(1);
   });
