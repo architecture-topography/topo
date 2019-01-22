ebf705adca168048d384be5dd0625741d19a5da0
Joey Hotz <joeyhotz1@gmail.com>
2019-01-22 11:46:26 +1100

    [Joey] adds methods to access Neo4j information

--- server/src/neo.ts
+++ server/src/neo.ts
@@ -1,10 +1,27 @@
 import * as Neo4j from "neo4j-driver";
 
+const getNeo4jUrl = () => {
+  if (!process.env.NEO4J_URL) {
+    throw new Error("Neo4j URL is undefined.");
+  }
+  return process.env.NEO4J_URL;
+};
+
+const getNeo4jCredentials = () => {
+  if (!process.env.NEO4J_USER || !process.env.NEO4J_PASSWORD) {
+    throw new Error("Either Neo4j user or password is undefined.");
+  }
+  return {
+    user: process.env.NEO4J_USER,
+    password: process.env.NEO4J_PASSWORD
+  };
+};
+
 export const driver = Neo4j.v1.driver(
-  process.env.NEO4J_URL + "",
+  getNeo4jUrl(),
   Neo4j.v1.auth.basic(
-    process.env.NEO4J_USER + "",
-    process.env.NEO4J_PASSWORD + ""
+    getNeo4jCredentials().user,
+    getNeo4jCredentials().password
   )
 );
 
