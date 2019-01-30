be5d2fbf74ebba45f223f02fdbe44989cf2479b2
Michelle Erica <michelle.lo@thoughtworks.com>
2019-01-30 15:25:34 +1100

    [Linus / Michelle] Add SystemItem Component

--- client/styleguide.config.js
+++ client/styleguide.config.js
@@ -1,4 +1,9 @@
+const path = require("path");
+
 module.exports = {
   propsParser: require("react-docgen-typescript").parse,
-  webpackConfig: require("react-scripts/config/webpack.config.js")
+  webpackConfig: require("react-scripts/config/webpack.config.js"),
+  require: [
+    path.join(__dirname, "../node_modules/semantic-ui-css/semantic.css")
+  ]
 };
