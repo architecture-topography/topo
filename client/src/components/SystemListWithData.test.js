33c911899dd0176d2fda956649e60d01200a7669
Michelle Erica <michelle.lo@thoughtworks.com>
2019-01-31 11:56:32 +1100

    [Linus / Michelle] Add SystemListWithData component

---
+++ client/src/components/SystemListWithData.test.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { mount } from "enzyme";
+import { MockedProvider } from "react-apollo/test-utils";
+import SystemListWithData, { GET_SYSTEMS } from "./SystemListWithData";
+const wait = require("waait");
+
+describe("SystemListWithData", () => {
+  const capabilityId = "capability-001";
+  const systems = [
+    {
+      id: "test-01",
+      name: "LeaveOz"
+    },
+    {
+      id: "test-02",
+      name: "LeaveOz2"
+    }
+  ];
+
+  const mocks = [
+    {
+      request: {
+        query: GET_SYSTEMS,
+        variables: { capabilityId }
+      },
+      result: {
+        data: {
+          systems
+        }
+      }
+    }
+  ];
+  it("Should show loading text while getting data", () => {
+    const root = mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SystemListWithData capabilityId={capabilityId} />
+      </MockedProvider>
+    );
+    expect(root.text()).toEqual("Loading...");
+  });
+
+  it("Should show error in UI", async () => {
+    const errorMock = {
+      ...mocks[0],
+      error: new Error("Error! Getting systems")
+    };
+
+    const root = mount(
+      <MockedProvider mocks={[errorMock]} addTypeName={false}>
+        <SystemListWithData capabilityId={capabilityId} />
+      </MockedProvider>
+    );
+
+    await wait(0);
+    root.update();
+
+    expect(root.text()).toEqual("Error! Getting systems");
+  });
+
+  it("Should pass data correctly on success", async () => {
+    const root = mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SystemListWithData capabilityId={capabilityId} />
+      </MockedProvider>
+    );
+
+    await wait(0);
+    root.update();
+
+    expect(root.find("SystemList").props().systems).toEqual(systems);
+  });
+});
---
+++ client/src/components/SystemListWithData.tsx
@@ -0,0 +1,28 @@
+import gql from "graphql-tag";
+import React from "react";
+import { Query } from "react-apollo";
+import SystemList from "./SystemList";
+
+export const GET_SYSTEMS = gql`
+  query Systems($capabilityId: String!) {
+    systems(capabilityId: $capabilityId) {
+      name
+      id
+    }
+  }
+`;
+
+const SystemListWithData = (props: { capabilityId: string }) => {
+  return (
+    <Query query={GET_SYSTEMS} variables={{ capabilityId: props.capabilityId }}>
+      {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error! Getting systems`;
+
+        return <SystemList systems={data.systems} />;
+      }}
+    </Query>
+  );
+};
+
+export default SystemListWithData;
