515a3c7b66be8e2a247950ea10d235ff2d66ec8f
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-06-04 10:38:08 +1000

    [Evan + Joe] :hocho:  client dir

--- client/styleguide.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  propsParser: require('react-docgen-typescript').parse,
-  require: [
-    path.join(__dirname, '../node_modules/semantic-ui-css/semantic.css'),
-  ],
-  webpackConfig: require('react-scripts/config/webpack.config.js'),
-};
